82b476f31dd0121e49721d287a1a069ca9bbbdff
Евгений Сергеев <sergeevem@rubezh.ru>
2024-03-01 22:07:47 +0400

    feat: add header scroll

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,3 +6,24 @@ import './ymap';
 import './edge';
 
 window.bootstrap = bootstrap;
+
+// плавающий header
+let lastScroll = 0;
+const defaultOffset = 200;
+const header = document.querySelector('.header');
+
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains('hide');
+
+window.addEventListener('scroll', () => {
+	if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
+		//scroll down
+		header.classList.add('hide');
+	}
+	else if(scrollPosition() < lastScroll && containHide()){
+		//scroll up
+		header.classList.remove('hide');
+	}
+
+	lastScroll = scrollPosition();
+});
